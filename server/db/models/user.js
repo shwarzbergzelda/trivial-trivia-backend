930ac96f846f51d94d0c6d2bdea0b8aa228dc6af
Wong <hwongkf8@gmail.com>
2022-01-26 15:18:49 -0500

    fix

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -22,16 +22,16 @@ const User = database.define("user", {
     },
   },
 
-  salt: {
-    type: Sequelize.STRING,
-    get() {
-      return () => this.getDataValue("salt");
-    },
-  },
+  // salt: {
+  //   type: Sequelize.STRING,
+  //   get() {
+  //     return () => this.getDataValue("salt");
+  //   },
+  // },
 
-  googleId: {
-    type: Sequelize.STRING,
-  },
+  // googleId: {
+  //   type: Sequelize.STRING,
+  // },
 
   bestCategory: {
     type: Sequelize.TEXT,
@@ -52,30 +52,30 @@ const User = database.define("user", {
   },
 });
 
-User.generateSalt = function () {
-  return crypto.randomBytes(16).toString("base64");
-};
+// User.generateSalt = function () {
+//   return crypto.randomBytes(16).toString("base64");
+// };
 
-User.encryptPassword = function (plainText, salt) {
-  return crypto
-    .createHash("RSA-SHA256")
-    .update(plainText)
-    .update(salt)
-    .digest("hex");
-};
+// User.encryptPassword = function (plainText, salt) {
+//   return crypto
+//     .createHash("RSA-SHA256")
+//     .update(plainText)
+//     .update(salt)
+//     .digest("hex");
+// };
 
-User.prototype.correctPassword = function (candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
-};
+// User.prototype.correctPassword = function (candidatePwd) {
+//   return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+// };
 
-const setSaltAndPassword = (user) => {
-  if (user.changed("password")) {
-    user.salt = User.generateSalt();
-    user.password = User.encryptPassword(user.password(), user.salt());
-  }
-};
+// const setSaltAndPassword = (user) => {
+//   if (user.changed("password")) {
+//     user.salt = User.generateSalt();
+//     user.password = User.encryptPassword(user.password(), user.salt());
+//   }
+// };
 
-User.beforeCreate(setSaltAndPassword);
-User.beforeUpdate(setSaltAndPassword);
+// User.beforeCreate(setSaltAndPassword);
+// User.beforeUpdate(setSaltAndPassword);
 
 module.exports = User;
